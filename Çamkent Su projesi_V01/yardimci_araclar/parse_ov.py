2926551f087ddaa1e1e5dd01d4f6d3468db309a9
limitpozitif <limitpozitif@gmail.com>
2026-08-07 21:40:24 +0300

    Delete Çamkent Su projesi_V01 directory

--- Çamkent Su projesi_V01/yardimci_araclar/parse_ov.py
+++
@@ -1,4 +0,0 @@
-import re
-dom = open(r'C:\Users\limit\AppData\Local\Temp\opencode\klavuz_dom.txt', encoding='utf-8', errors='replace').read()
-for m in re.finditer(r'id="(ov\d+)"[^>]*>([^<]+)<', dom):
-    print(m.group(1), '->', m.group(2))
